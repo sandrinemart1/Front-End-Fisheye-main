3db46c042a5b3c2b8166398dc2ef1e94b54fa2c0
sandrinemart1 <sandrinestreicher@gmail.com>
2022-10-31 22:00:51 +0100

    %scrollY(index.js)

--- module/index.js
+++ module/index.js
@@ -5,13 +5,13 @@ import { myFetch  } from './fetch.js'
 myFetch()
 
 ////////evenement scrollY pour atteindre le contenu////////////
-window.addEventListener('scroll', () => content()) 
+// window.addEventListener('scroll', () => content()) 
 
-function content(){
-    const pageContent = document.querySelector('.contenu_link')
-    if ( window.scrollY > 250 ) {
-        pageContent.style.display = 'flex'
-      } else {
-        pageContent.style.display = 'none'
-      }
-}
+// function content(){
+//     const pageContent = document.querySelector('.contenu_link')
+//     if ( window.scrollY > 250 ) {
+//         pageContent.style.display = 'flex'
+//       } else {
+//         pageContent.style.display = 'none'
+//       }
+// }
